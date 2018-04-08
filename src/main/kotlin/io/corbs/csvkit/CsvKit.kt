83f516e1dbe0c66da229854d51bb5ce031076c4d
Corbett Martin <corbtastik@gmail.com>
2018-04-08 16:08:45 -0500

    adding app info with version

--- src/main/kotlin/io/corbs/csvkit/CsvKit.kt
+++ src/main/kotlin/io/corbs/csvkit/CsvKit.kt
@@ -1,5 +1,6 @@
 package io.corbs.csvkit
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.CacheManager
@@ -9,12 +10,15 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.netflix.hystrix.EnableHystrix
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.context.annotation.Bean
+import java.net.InetAddress
 
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
 @SpringBootApplication
 class CsvKit {
+
+    companion object { val LOG = LoggerFactory.getLogger(CsvKit::class.java.name) }
 
     @Bean
     fun cacheManager(): CacheManager {
@@ -27,7 +31,25 @@ class CsvKit {
 }
 
 fun main(args: Array<String>) {
-    runApplication<CsvKit>(*args)
+
+    val env = runApplication<CsvKit>(*args).environment
+    var protocol = "http"
+    if (env.getProperty("server.ssl.key-store") != null) {
+        protocol = "https"
+    }
+    CsvKit.LOG.info("\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n\t" +
+        "Application '{}' is running! Access URLs:\n\t" +
+        "Local: \t\t{}://localhost:{}\n\t" +
+        "External: \t{}://{}:{}\n\t" +
+        "Profile(s): \t{}\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>",
+        env.getProperty("spring.application.name"),
+        protocol,
+        env.getProperty("server.port"),
+        protocol,
+        InetAddress.getLocalHost().hostAddress,
+        env.getProperty("server.port"),
+        env.activeProfiles)
+
 }
 
 
